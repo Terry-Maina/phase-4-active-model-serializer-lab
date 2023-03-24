0238996c9560ffc433c56880184a326c55d58f07
Teresa <tenjerim@gmail.com>
2023-03-24 13:53:06 +0300

    completed

--- app/serializers/short_content_serializer.rb
+++ app/serializers/short_content_serializer.rb
@@ -1,3 +1,7 @@
 class ShortContentSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :title, :short_content, :tags
+    
+    def short_content
+        "#{self.object.content[0..39]}..."
+    end
 end
